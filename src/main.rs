9f6ad2033e9bff91f07778d4f36d5587eb26a5f3
KingTariq <tariqmasood2k2@yahoo.com>
2019-12-15 18:12:53 +0500

    first comment

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let team = ["Pakistan","Srilanka","India","England","Australia"];
+    let year  = [1992,1996,2000,2004,2008];
+    println!("Cricket Team:{} _ Year:{}",team[0],year[0]);
+
+    println!("Cricket Team:{} _ Year:{}",team[1],year[1]);
+
+    println!("Cricket Team:{} _ Year:{}",team[2],year[2]);
+
+    println!("Cricket Team:{} _ Year:{}",team[3],year[3]);
+
+    println!("Cricket Team:{} _ Year:{}",team[4],year[4]);
+}
