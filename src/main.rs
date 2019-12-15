a453d07c0d22d2fd09d7dce5f8ae184097759d13
KingTariq <tariqmasood2k2@yahoo.com>
2019-12-15 18:15:23 +0500

    sss

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let year  = [1992,1996,2000,2004,2008];
     println!("Cricket Team:{} _ Year:{}",team[0],year[0]);
 
-    println!("Cricket Team:{} _ Year:{}",team[1],year[1]);
+    println!("Cricket Team:{} _ abcd Year:{}",team[1],year[1]);
 
     println!("Cricket Team:{} _ Year:{}",team[2],year[2]);
 
